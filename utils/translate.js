37d9af11e12939eb3575d5f82b7beb70c14397bd
yuhuan9971 <yuhuan9971@users.noreply.github.com>
2022-02-03 19:07:41 +0800

    remove unexpected question mark

--- utils/translate.js
+++ utils/translate.js
@@ -3,7 +3,7 @@ const translate = require('@vitalets/google-translate-api');
 module.exports = function(text, options) {
     return new Promise((resolve, reject)=> {
         // translate is case sensitive
-        if (options?.to == 'zh-tw') options?.to = 'zh-TW'
+        if (options?.to == 'zh-tw') options.to = 'zh-TW'
         translate(text, options).then(res => {
             resolve(res)
         }).catch(err => {

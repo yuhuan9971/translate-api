54625b78168d00525fb6d173d0b97f26ecdf46ce
yuhuan9971 <yuhuan9971@users.noreply.github.com>
2022-02-03 13:06:31 +0800

    fix translate case sensitive problem

--- utils/translate.js
+++ utils/translate.js
@@ -2,6 +2,8 @@ const translate = require('@vitalets/google-translate-api');
 
 module.exports = function(text, options) {
     return new Promise((resolve, reject)=> {
+        // translate is case sensitive
+        if (text == 'zh-tw') text = 'zh-TW'
         translate(text, options).then(res => {
             resolve(res)
         }).catch(err => {
